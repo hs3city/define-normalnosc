b3c539579035a862138a412682892d869f6e105e
maknetaRo <makneta@gmail.com>
2022-03-06 20:44:14 +0100

    add functionality to menu button, alts to images and navbar to prelegents subpages

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 console.log('hello world');
 // navigation
+
+const toggleBtn = document.querySelector('.nav-toggle');
+const navList = document.querySelector('.nav-list');
+const menu = document.querySelector('.menu');
+
+toggleBtn.addEventListener('click', function () {
+  navList.classList.toggle('show-links');
+  menu.classList.toggle('menu-mobile');
+});
