e32e2e132b8138d4d44639797e635af19c7bea48
maknetaRo <makneta@gmail.com>
2022-03-13 19:38:09 +0100

    change icon for external links, add Simon Monlisnki's social links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-console.log('hello world');
 // navigation
 
 const toggleBtn = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
 const menu = document.querySelector('.menu');
 const navItems = document.querySelectorAll('.nav-item');
-console.log(navItems);
 
 toggleBtn.addEventListener('click', function () {
   navList.classList.toggle('show-links');
