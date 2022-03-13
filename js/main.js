1236d5c8ad6fafdb864ed1d92b3a4ebc5f6956e5
Magdalena <makneta@gmail.com>
2022-03-13 22:11:03 +0100

    Project setup (#14)
    * change icon for external links, add Simon Monlisnki's social links

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
-console.log('hello world');
 // navigation
 
 const toggleBtn = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
 const menu = document.querySelector('.menu');
 const navItems = document.querySelectorAll('.nav-item');
+<<<<<<< HEAD
+=======
 console.log(navItems);
+>>>>>>> 257a5c406f5d6b29c75f8ec83aeec80791d8a674
 
 toggleBtn.addEventListener('click', function () {
   navList.classList.toggle('show-links');
