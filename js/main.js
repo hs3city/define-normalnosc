5df2e162555050706027a044e29e193ca19a2848
maknetaRo <makneta@gmail.com>
2022-03-13 13:40:07 +0100

    add speakers social media links

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@ console.log('hello world');
 const toggleBtn = document.querySelector('.nav-toggle');
 const navList = document.querySelector('.nav-list');
 const menu = document.querySelector('.menu');
+const navItems = document.querySelectorAll('.nav-item');
+console.log(navItems);
 
 toggleBtn.addEventListener('click', function () {
   navList.classList.toggle('show-links');
   menu.classList.toggle('menu-mobile');
+});
+
+navItems.forEach(function (item) {
+  item.addEventListener('click', function () {
+    navList.classList.toggle('show-links');
+    menu.classList.toggle('menu-mobile');
+  });
 });
